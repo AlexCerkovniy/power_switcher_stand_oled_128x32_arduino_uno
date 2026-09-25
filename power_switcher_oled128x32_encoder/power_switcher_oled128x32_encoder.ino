0c0285fbbad359a5d3b9dca76a9a779ff093fca7
Alexander <tserkovniy.o@ajax.systems>
2026-09-25 16:06:04 +0300

    Add buttons

--- power_switcher_oled128x32_encoder/power_switcher_oled128x32_encoder.ino
+++ power_switcher_oled128x32_encoder/power_switcher_oled128x32_encoder.ino
@@ -2,17 +2,50 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <EncButton.h>
 
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+/* Uncomment to use buttons instead of encoder */
+#define USE_BUTTONS
+
+#ifndef USE_BUTTONS
+#include <EncButton.h>
+#endif
+
+#define OLED_RESET     4
+#define SCREEN_ADDRESS 0x3C
+
 Adafruit_SSD1306 display(128, 32, &Wire, OLED_RESET);
 
-EncButton<EB_CALLBACK, 2, 3, 4> enc;   // энкодер с кнопкой <A, B, KEY>
+/*
+ * Encoder pins:
+ *   A   = 2
+ *   B   = 3
+ *   KEY = 4
+ *
+ * Buttons mode:
+ *   PLUS  = 2
+ *   MINUS = 3
+ *   OK    = 4
+ */
 
-#define OUTPUT_PIN  (8)
+#define INPUT_PIN_PLUS   2
+#define INPUT_PIN_MINUS  3
+#define INPUT_PIN_OK     4
+
+#ifndef USE_BUTTONS
+EncButton<EB_CALLBACK, INPUT_PIN_PLUS, INPUT_PIN_MINUS, INPUT_PIN_OK> enc;
+#else
+typedef struct {
+  uint8_t pin;
+  bool last_raw_state;
+  bool stable_state;
+  uint32_t debounce_timer;
+} button_t;
+#endif
+
+#define OUTPUT_PIN 8
 
 bool draw = false;
+
 uint16_t period_ms = 12000;
 uint16_t pause_ms = 1000;
 
@@ -31,38 +64,23 @@ bool output_enabled = true;
 
 uint32_t millis_prev = 0;
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(OUTPUT_PIN, OUTPUT);
 
-  enc.attach(RIGHT_HANDLER, encRight);
-  enc.attach(LEFT_HANDLER, encLeft);
-  enc.attach(PRESS_HANDLER, encPress);
+/* -------------------------------------------------------------------------- */
+/* Common control handlers                                                    */
+/* -------------------------------------------------------------------------- */
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(1000);
-  draw = true;
-}
-
-void encRight() {
-  if(param == SELECTED_PERIOD){
+void controlPlus() {
+  if (param == SELECTED_PERIOD) {
     period_ms += 100;
   }
-  else if(param == SELECTED_PAUSE){
+  else if (param == SELECTED_PAUSE) {
     pause_ms += 100;
   }
-  else if(param == SELECTED_STATE){
+  else if (param == SELECTED_STATE) {
     output_enabled = true;
     output = true;
     output_timer = pause_ms;
+
     digitalWrite(LED_BUILTIN, HIGH);
     digitalWrite(OUTPUT_PIN, HIGH);
   }
@@ -70,20 +88,22 @@ void encRight() {
   draw = true;
 }
 
-void encLeft() {
-  if(param == SELECTED_PERIOD){
-    if(period_ms >= 100){
+
+void controlMinus() {
+  if (param == SELECTED_PERIOD) {
+    if (period_ms >= 100) {
       period_ms -= 100;
     }
   }
-  else if(param == SELECTED_PAUSE){
-    if(pause_ms >= 100){
+  else if (param == SELECTED_PAUSE) {
+    if (pause_ms >= 100) {
       pause_ms -= 100;
     }
   }
-  else if(param == SELECTED_STATE){
+  else if (param == SELECTED_STATE) {
     output_enabled = false;
     output = false;
+
     digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(OUTPUT_PIN, LOW);
   }
@@ -91,64 +111,245 @@ void encLeft() {
   draw = true;
 }
 
-void encPress() {
-  if(param == SELECTED_NONE){
+
+void controlOk() {
+  if (param == SELECTED_NONE) {
     param = SELECTED_PERIOD;
   }
-  else if(param == SELECTED_PERIOD){
+  else if (param == SELECTED_PERIOD) {
     param = SELECTED_PAUSE;
   }
-  else if(param == SELECTED_PAUSE){
+  else if (param == SELECTED_PAUSE) {
     param = SELECTED_STATE;
   }
-  else if(param == SELECTED_STATE){
+  else if (param == SELECTED_STATE) {
     param = SELECTED_NONE;
   }
-  
+
   draw = true;
 }
 
-void loop() {
-  if(millis() - millis_prev >= 100){
-    millis_prev = millis();
-    
-    if(output_enabled){
-      if(output){
-          if(output_timer > 100){
-            output_timer -= 100;
-          }
-          else{
-            output = false;
-            digitalWrite(LED_BUILTIN, LOW);
-            digitalWrite(OUTPUT_PIN, LOW);
-            output_timer = period_ms;
-          }
+
+/* -------------------------------------------------------------------------- */
+/* Encoder                                                                    */
+/* -------------------------------------------------------------------------- */
+
+#ifndef USE_BUTTONS
+
+void encRight() {
+  controlPlus();
+}
+
+
+void encLeft() {
+  controlMinus();
+}
+
+
+void encPress() {
+  controlOk();
+}
+
+#endif
+
+
+/* -------------------------------------------------------------------------- */
+/* Buttons                                                                    */
+/* -------------------------------------------------------------------------- */
+
+#ifdef USE_BUTTONS
+
+#define BUTTON_DEBOUNCE_MS 30
+
+button_t button_plus = {
+  INPUT_PIN_PLUS,
+  HIGH,
+  HIGH,
+  0
+};
+
+button_t button_minus = {
+  INPUT_PIN_MINUS,
+  HIGH,
+  HIGH,
+  0
+};
+
+button_t button_ok = {
+  INPUT_PIN_OK,
+  HIGH,
+  HIGH,
+  0
+};
+
+bool buttonPressed(button_t *button) {
+  bool raw_state = digitalRead(button->pin);
+
+  if (raw_state != button->last_raw_state) {
+    button->last_raw_state = raw_state;
+    button->debounce_timer = millis();
+  }
+
+  if ((millis() - button->debounce_timer) >= BUTTON_DEBOUNCE_MS) {
+    if (raw_state != button->stable_state) {
+      button->stable_state = raw_state;
+
+      /*
+       * INPUT_PULLUP:
+       * LOW = button pressed
+       */
+      if (button->stable_state == LOW) {
+        return true;
       }
-      else{
-        if(output_timer > 100){
+    }
+  }
+
+  return false;
+}
+
+void buttonsTick() {
+  if (buttonPressed(&button_plus)) {
+    controlPlus();
+  }
+
+  if (buttonPressed(&button_minus)) {
+    controlMinus();
+  }
+
+  if (buttonPressed(&button_ok)) {
+    controlOk();
+  }
+}
+
+#endif
+
+
+/* -------------------------------------------------------------------------- */
+/* Setup                                                                      */
+/* -------------------------------------------------------------------------- */
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(OUTPUT_PIN, OUTPUT);
+
+#ifdef USE_BUTTONS
+
+  /*
+   * Buttons are expected to connect pin to GND when pressed.
+   */
+  pinMode(INPUT_PIN_PLUS, INPUT_PULLUP);
+  pinMode(INPUT_PIN_MINUS, INPUT_PULLUP);
+  pinMode(INPUT_PIN_OK, INPUT_PULLUP);
+
+#else
+
+  enc.attach(RIGHT_HANDLER, encRight);
+  enc.attach(LEFT_HANDLER, encLeft);
+  enc.attach(PRESS_HANDLER, encPress);
+
+#endif
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+
+    for (;;) {
+    }
+  }
+
+  display.display();
+  delay(1000);
+
+  draw = true;
+}
+
+
+/* -------------------------------------------------------------------------- */
+/* Loop                                                                       */
+/* -------------------------------------------------------------------------- */
+
+void loop() {
+  if (millis() - millis_prev >= 100) {
+    millis_prev = millis();
+
+    if (output_enabled) {
+      if (output) {
+        if (output_timer > 100) {
           output_timer -= 100;
         }
-        else{
+        else {
+          output = false;
+
+          digitalWrite(LED_BUILTIN, LOW);
+          digitalWrite(OUTPUT_PIN, LOW);
+
+          output_timer = period_ms;
+        }
+      }
+      else {
+        if (output_timer > 100) {
+          output_timer -= 100;
+        }
+        else {
           output = true;
+
           digitalWrite(LED_BUILTIN, HIGH);
           digitalWrite(OUTPUT_PIN, HIGH);
+
           output_timer = pause_ms;
         }
       }
     }
   }
-  
-  if(draw){
+
+
+  if (draw) {
     draw = false;
+
     display.clearDisplay();
-    display.setTextSize(1);             
+
+    display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
+
     display.setCursor(0, 0);
-    if(param == SELECTED_PERIOD) display.print(F(">")); display.print(F("Period:")); display.print(period_ms, DEC); display.println(F("ms"));
-    if(param == SELECTED_PAUSE) display.print(F(">")); display.print(F("Pause:")); display.print(pause_ms, DEC); display.println(F("ms"));
-    if(param == SELECTED_STATE) display.print(F(">")); if(output_enabled) display.print(F("State: On")); else display.println(F("State: Off"));
+
+    if (param == SELECTED_PERIOD) {
+      display.print(F(">"));
+    }
+
+    display.print(F("Period:"));
+    display.print(period_ms, DEC);
+    display.println(F("ms"));
+
+
+    if (param == SELECTED_PAUSE) {
+      display.print(F(">"));
+    }
+
+    display.print(F("Pause:"));
+    display.print(pause_ms, DEC);
+    display.println(F("ms"));
+
+
+    if (param == SELECTED_STATE) {
+      display.print(F(">"));
+    }
+
+    if (output_enabled) {
+      display.println(F("State: On"));
+    }
+    else {
+      display.println(F("State: Off"));
+    }
+
     display.display();
   }
 
+
+#ifdef USE_BUTTONS
+  buttonsTick();
+#else
   enc.tick();
+#endif
 }
